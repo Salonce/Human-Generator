9ab3ef77ef7dad926f0f4a80c6f185b70f3de439
Saloncey <jakukaj@gmail.com>
2024-07-03 13:00:42 +0200

    modified: modiefied sd in getAdjusted()

--- Traits/Cognition/Stratum2/Default.py
+++ Traits/Cognition/Stratum2/Default.py
@@ -4,4 +4,4 @@ def getDefault():
     return random.normal(loc=100, scale=15, size=(1))
 
 def getAdjusted(general_intelligence):
-    return random.normal(loc=general_intelligence, scale=15, size=(1))
+    return random.normal(loc=general_intelligence, scale=6, size=(1))
